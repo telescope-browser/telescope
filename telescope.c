714b1fcb787b10697bbe286d72c832281f09dec3
Omar Polo <op@omarpolo.com>
2022-01-02 08:38:37 +0000

    fix crash when --color is specified
    found and reported by Florian, thanks!

--- telescope.c
+++ telescope.c
@@ -36,7 +36,7 @@
 #include "ui.h"
 
 static struct option longopts[] = {
-	{"colors",	no_argument,	NULL,	'c'},
+	{"colors",	no_argument,	NULL,	'C'},
 	{"help",	no_argument,	NULL,	'h'},
 	{"safe",	no_argument,	NULL,	'S'},
 	{"version",	no_argument,	NULL,	'v'},
